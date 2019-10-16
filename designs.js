491718ec40e953c2c2cf9a9dc93625653f5ae8cd
Muhammad Reda Abdelaal <trans.muhammad@gmail.com>
2019-10-17 01:26:28 +0200

    adding functionlaty for coloring the cells on click

--- designs.js
+++ designs.js
@@ -1,16 +1,32 @@
-// Selecting DOM elements
+
+// Selecting DOM elements using select function created below
 const colorInput = select("colorPicker");
 const inputHeight = select("inputHeight");
 const inputWidth = select("inputWidth");
 const submit = select("submit");
 const pixelCanvas = select("pixelCanvas");
+let color = "#000"; //set a default value for color if user did not select new one
 
-
-// on Click submit button call makeGrid function
-submit.addEventListener("click", function(e){
-  e.preventDefault();    // do not referesh the page
+/* on Click submit button 
+1st: do not refresh the page
+2nd: call makeGrid function
+3rd: call colorOnClick function
+*/
+submit.addEventListener("click", e => {
+  e.preventDefault(); // do not refresh the page
   makeGrid();
+  colorOnClick();
 });
+
+
+/* 
+  All functions
+*/
+
+// select a DOM element by ID
+function select(id) {
+  return document.getElementById(id); // returns the selected DOM element
+}
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
@@ -28,17 +44,35 @@ function makeGrid() {
   
   // prepare the rows
   let rows = `<tr>${cols}</tr>`;
-  for(let i = 0; i < height; i++) {
-    rows += rows;
+  for(let i = 1; i < height; i++) {
+    rows += `<tr>${cols}</tr>`;
   }
   
-  // fill the canvas with rows where every row contains the number of selected columns
+  // fill the canvas table with rows where every row contains the number of selected columns
   pixelCanvas.innerHTML = rows;
 }
 
-// select a DOM element by ID
-function select(id) {
-  return document.getElementById(id);
+// color the cells on click
+function colorOnClick() {
+  // update the color on user input
+  colorInput.addEventListener("input", () => {
+    color = colorInput.value;
+  });
+
+  // save all cells in an array
+  let allCells = document.querySelectorAll("#pixelCanvas td");
+
+  // add event listener for each cell inside allCells Array
+  allCells.forEach(cell => {
+    cell.addEventListener("click", e => {   // on Click any cell
+      if(e.target.style.backgroundColor == "") { // if the cell has no color yet
+        e.target.style.backgroundColor = color; // color it with the selected color value
+      } else { // else
+        e.target.style.backgroundColor = ""; // remove the current color if clicked again
+      }
+    });
+  });
 }
+
 
 
