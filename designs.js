1d5d5ca5105c4b520eb778897f67c6b854b3d6c4
Muhammad Reda Abdelaal <trans.muhammad@gmail.com>
2019-10-16 23:36:19 +0200

    adding fucntionallity for making grid on submit

--- designs.js
+++ designs.js
@@ -1,9 +1,44 @@
-// Select color input
-// Select size input
+// Selecting DOM elements
+const colorInput = select("colorPicker");
+const inputHeight = select("inputHeight");
+const inputWidth = select("inputWidth");
+const submit = select("submit");
+const pixelCanvas = select("pixelCanvas");
+
+
+// on Click submit button call makeGrid function
+submit.addEventListener("click", function(e){
+  e.preventDefault();    // do not referesh the page
+  makeGrid();
+});
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
-  // Your code goes here!
+  pixelCanvas.innerHTML = ""; // clear the canvas before creating any new cells
+
+  // get value inputs
+  let height =  inputHeight.value;
+  let width =  inputWidth.value;
+  
+  // prepare the columns
+  let cols = "<td></td>";
+  for (let i = 1; i < width; i++)   {
+    cols += "<td></td>"; 
+  }
+  
+  // prepare the rows
+  let rows = `<tr>${cols}</tr>`;
+  for(let i = 0; i < height; i++) {
+    rows += rows;
+  }
+  
+  // fill the canvas with rows where every row contains the number of selected columns
+  pixelCanvas.innerHTML = rows;
 }
+
+// select a DOM element by ID
+function select(id) {
+  return document.getElementById(id);
+}
+
 
